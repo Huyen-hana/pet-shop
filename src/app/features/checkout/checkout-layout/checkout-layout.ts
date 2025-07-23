b9445ed97e815764add801978bf5ec802d19ee14
Huyen <huyen.hana2208@gmail.com>
2025-07-23 22:38:08 +0700

    Set up file structure for demo project

---
+++ src/app/features/checkout/checkout-layout/checkout-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout-layout',
+  imports: [],
+  templateUrl: './checkout-layout.html',
+  styleUrl: './checkout-layout.scss'
+})
+export class CheckoutLayout {
+
+}
