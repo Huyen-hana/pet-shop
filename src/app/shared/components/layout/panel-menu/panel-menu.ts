5378c79c9b58759207e8000288d0e0601070caed
Huyen <huyen.hana2208@gmail.com>
2025-07-28 00:48:08 +0700

    COM-7 create meagmenu, panel menu for header. Add drawer component for for tablets and mobiles, define category and categoryItem interface in common.model.ts

---
+++ src/app/shared/components/layout/panel-menu/panel-menu.ts
@@ -0,0 +1,58 @@
+import { Component, Input } from '@angular/core';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-panel-menu',
+  imports: [PanelMenuModule],
+  templateUrl: './panel-menu.html',
+  styleUrl: './panel-menu.scss'
+})
+export class PanelMenu {
+  menu: MenuItem[] = [];
+
+  @Input() category: any[] = [];
+
+  ngOnInit(): void {
+    console.log(this.category)
+    this.renderPanelMenu();
+  }
+
+  renderPanelMenu() {
+    this.menu = (this.category || []).map(parent => ({
+      label: parent.cateId,
+      items: (parent.cateNameList || []).map((nameItem: any) => {
+        const label = Array.isArray(nameItem.cateName)
+          ? nameItem.cateName[0]
+          : nameItem.cateName;
+  
+        const hasChild = Array.isArray(nameItem.cateChild) && nameItem.cateChild.length > 0;
+  
+        return hasChild
+          ? {
+              label,
+              items: nameItem.cateChild.map((child: any) => ({
+                label: child,
+                routerLink: ['/main/collections']
+              }))
+            }
+          : { label,
+              routerLink: ['/main/collections']
+            };
+      })
+    }));
+  
+  }
+
+  //define color scheme for amber panel
+  amberPanel = {
+    colorScheme: {
+      light: {
+        item: {
+          focusColor: '#b45309',
+          focusBackground: '#fffbeb',
+        }
+      }
+    }
+  }
+}
