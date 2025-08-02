64dd115e79728a59b8ce622ed15b26aba8eb3f6e
Huyen <huyen.hana2208@gmail.com>
2025-08-03 03:17:53 +0700

    COM-11 update main.routes.ts, add register and forgot-password. Update main-layout.html and home.html

--- src/app/features/main/main.routes.ts
+++ src/app/features/main/main.routes.ts
@@ -13,6 +13,14 @@ export const MAIN_ROUTES: Routes = [
             {
                 path: 'home',
                 loadComponent: () => import('../main/pages/home/home').then(m => m.Home)
+            },
+            {
+                path: 'auth/register',
+                loadComponent: () => import('../main/pages/auth/register/register').then(m => m.Register)
+            },
+            {
+                path: 'auth/forgot-password',
+                loadComponent: () => import('../main/pages/auth/forgot-password/forgot-password').then(m => m.ForgotPassword)
             }
         ]
 
