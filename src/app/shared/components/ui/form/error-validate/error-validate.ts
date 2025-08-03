9e23451d58f3ffd1a3886a49537051d5e31169e3
Huyen <huyen.hana2208@gmail.com>
2025-08-04 03:50:14 +0700

    COM-12 Update register, user drawer and error-validate

--- src/app/shared/components/ui/form/error-validate/error-validate.ts
+++ src/app/shared/components/ui/form/error-validate/error-validate.ts
@@ -22,9 +22,9 @@ export class ErrorValidate {
   }
   getErrMess(keyErr: string, value: any) {
     let messages = {
-      'required': `${this.nameControl} is required field`,
-      'minlength': `Please enter at least ${value.requiredLength} characters`,
-      'pattern': `${this.nameControl} is incorrect format`
+      'required': `${this.nameControl} là trường bắt buộc`,
+      'minlength': `Hãy nhập ít nhất ${value.requiredLength} ký tự`,
+      'pattern': `${this.nameControl} phải hợp lệ`
     }
     return messages[keyErr as keyof typeof messages];
   }
