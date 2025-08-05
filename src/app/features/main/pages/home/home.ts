690e5a9f0b271aa8a6e8374d0094f761e1cc14b9
Huyen <huyen.hana2208@gmail.com>
2025-08-06 04:35:23 +0700

    COM-14 Update home page with cards, update user-drawer.html

--- src/app/features/main/pages/home/home.ts
+++ src/app/features/main/pages/home/home.ts
@@ -1,15 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SocialBanner } from '../../../../shared/components/bussiness/social-banner/social-banner';
 import { Photo } from '../../../../shared/services/business/photo-service';
 import { RouterLink } from '@angular/router';
+import { ProductCard } from '../../../../shared/components/bussiness/products/product-card/product-card';
+import { ProductService } from '../../../../shared/services/product-service/product-service';
+import { Product } from '../../../../shared/models/product.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
-  imports: [SocialBanner, RouterLink],
+  imports: [SocialBanner, RouterLink, ProductCard],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
+export class Home implements OnInit {
   policies = [
     {
       title: 'Miễn Phí Vận Chuyển',
@@ -31,5 +35,29 @@ export class Home {
 
   private photoService = inject(Photo);
   brands = this.photoService.brands;
+
+  private productService = inject(ProductService);
+  subscript : Subscription | undefined;
+  products: Product[] = [];
+  lovedProducts: Product[] = [];
+  newProducts: Product[] = [];
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  };
+  ngOnDestroy(): void {
+    if (this.subscript) {
+      this.subscript.unsubscribe()
+    };
+  };
+
+  getAllProducts() {
+    this.subscript = this.productService.getAll().subscribe((products: Product[]) => {
+      this.products = products;
+
+      this.lovedProducts = products.slice(0, 10);
+      this.newProducts = products.slice(10, 20);
+    })
+  };
 
 }
