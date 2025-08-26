d24d5fa8d095aa468dc08c63d886961c7ecbeeb8
Huyen <huyen.hana2208@gmail.com>
2025-08-27 03:40:56 +0700

    COM-31 Update checkout.routes.ts and create checkout-layout component

--- src/app/features/checkout/checkout.routes.ts
+++ src/app/features/checkout/checkout.routes.ts
@@ -5,7 +5,15 @@ export const CHECKOUT_ROUTES: Routes = [
         path: '',
         loadComponent: () => import('../checkout/checkout-layout/checkout-layout').then(m => m.CheckoutLayout),
         children: [
-            
+            {
+                path: '',
+                redirectTo: 'confirmation',
+                pathMatch: 'full'
+            },
+            {
+                path: 'confirmation',
+                loadComponent: () => import('../checkout/pages/confirmation/confirmation').then(m => m.Confirmation)
+            }
         ]
     }
 ]
