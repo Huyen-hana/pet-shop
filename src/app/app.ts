ad464b1e9d69091d9223b2d5dcf7d87d8e3d1c7f
Huyen <huyen.hana2208@gmail.com>
2025-09-07 03:14:17 +0700

    COM-34 Update app.config.ts, app.html, admin.route.ts. Update admin-layout, admin-header, admin-sidebar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,20 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Toast } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { filter, Subject, takeUntil } from 'rxjs';
 import { LoadingOverlay } from "./shared/components/ui/loading-overlay/loading-overlay";
-import { AuthService } from './shared/services/auth-service/auth-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toast, LoadingOverlay],
+  imports: [RouterOutlet, Toast, ConfirmDialogModule, LoadingOverlay],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App implements OnInit, OnDestroy {
+export class App implements OnDestroy {
   protected title = 'pet-shop';
   private router = inject(Router);
   private destroy$ = new Subject<void>();
-  private authService = inject(AuthService);
 
   constructor() {
     this.router.events
@@ -26,28 +25,8 @@ export class App implements OnInit, OnDestroy {
       })
   };
 
-  ngOnInit(): void {
-    this.authService.loadUserFromLocalStorage();
-    this.redirectIfAdmin();
-  }
-
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
-  };
-  
-  private redirectIfAdmin(): void {
-    const userRaw = localStorage.getItem('currentUser');
-    const user = userRaw ? JSON.parse(userRaw) : null;
-
-    if (user?.role === 'admin') {
-      this.router.navigate(['/admin/dashboard']);
-    }
-  };
-
-  myAmberToast = {
-    root: {
-      width: '300px'
-    }
   };
 }
--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -15,6 +15,10 @@ export const ADMIN_ROUTES: Routes = [
             {
                 path: 'dashboard',
                 loadComponent: () => import('../admin/pages/dashboard/dashboard').then(m => m.Dashboard)
+            },
+            {
+                path: 'ui-kid/table/:name',
+                loadComponent: () => import('../admin/pages/products-management/products-management').then(m => m.ProductsManagement)
             }
         ]
     }
