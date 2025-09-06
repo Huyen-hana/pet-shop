28e2bf38d946063a61b6e0f73980002a56418d74
Huyen <huyen.hana2208@gmail.com>
2025-09-07 03:25:10 +0700

    COM-34 Update table component, cart-page.ts, cart-service and create dialog-manager

--- src/app/shared/components/ui/table/table.ts
+++ src/app/shared/components/ui/table/table.ts
@@ -1,19 +1,26 @@
 import { CommonModule, NgTemplateOutlet } from '@angular/common';
-import { Component, Input, TemplateRef } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { Table } from 'primeng/table';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { Product } from '../../../models/product.model';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-table',
-  imports: [NgTemplateOutlet, CommonModule, TableModule],
+  imports: [NgTemplateOutlet, CommonModule, TableModule, IconFieldModule, InputIconModule, InputTextModule, Button],
   templateUrl: './table.html',
   styleUrl: './table.scss'
 })
-export class Table {
-  @Input() data: any[] = [];
+export class myTable {
+  @Input() data!: any[];
   @Input() columns: { field: string; header: string; sortable?: boolean }[] = [];
 
   @Input() headerTemplate?:  TemplateRef<any>;
   @Input() rowTemplate?: TemplateRef<any>;
+  @Input() exRowTemplate?: TemplateRef<any>;
   @Input() footerTemplate?: TemplateRef<any>;
 
   @Input() paginator: boolean = false;
@@ -21,5 +28,32 @@ export class Table {
 
   @Input() sortable: boolean = false;
   @Input() searchable: boolean = false;
- 
+  @Input() dataKey: string = '';
+  @Input() captionMode: 'search' | 'expand' | 'none' = 'none';
+
+  @ViewChild('dt2') dt2!: Table;
+  selectedProducts!: Product;
+  expandedRows = {};
+
+  isImagePath(value: any): boolean {
+    if (typeof value !== 'string') return false;
+    return /\.(jpg|jpeg|png|gif|webp|svg)$/i.test(value);
+  };
+
+  clear(table: Table) {
+    table.clear();
+  };
+
+  onGlobalFilter(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const value = input?.value ?? '';
+    this.dt2.filterGlobal(value, 'contains');
+  };
+  expandAll() {
+    // dataKey: id
+    this.expandedRows = this.data.reduce((acc, p) => (acc[p.id] = true) && acc, {});
+  };
+  collapseAll() {
+    this.expandedRows = {};
+  };
 }
