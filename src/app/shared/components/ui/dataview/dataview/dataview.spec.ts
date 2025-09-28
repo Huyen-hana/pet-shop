14f459348c19776da5deb71d5ec085dea04b5664
Huyen <huyen.hana2208@gmail.com>
2025-09-29 05:24:01 +0700

    COM-41 unit test for dataview and cart-service

--- src/app/shared/components/ui/dataview/dataview/dataview.spec.ts
+++ src/app/shared/components/ui/dataview/dataview/dataview.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Dataview } from './dataview';
+import { MessageService } from 'primeng/api';
+import { CartService } from '../../../../services/cart-service/cart-service';
 
 describe('Dataview', () => {
   let component: Dataview;
   let fixture: ComponentFixture<Dataview>;
+  let cartSpy: jasmine.SpyObj<CartService>;
 
   beforeEach(async () => {
+    cartSpy = jasmine.createSpyObj<CartService>('CartService', [
+      'items',
+      'getItemQuantity',
+      'getTotalPrice'
+    ]);
+    cartSpy.items.and.returnValue([
+      { id: '1', name: 'hạt mèo', quantity: 2, price: 50, sale: 0, currentStock: 10 },
+      { id: '2', name: 'pate', quantity: 1, price: 60, sale: 0, currentStock: 5 }
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [Dataview]
+      imports: [Dataview],
+      providers: [
+        MessageService,
+        { provide: CartService, useValue: cartSpy },
+      ]
     })
     .compileComponents();
 
@@ -19,5 +36,12 @@ describe('Dataview', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return cart items from CartService', () => {
+    const products = component.products;
+    expect(products.length).toBe(2);
+    expect(products[0].name).toBe('hạt mèo');
+    expect(products[1].name).toBe('pate');
   });
 });
