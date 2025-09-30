ddcedcf21957363ff628e6b2dd99acad28e044bd
Huyen <huyen.hana2208@gmail.com>
2025-10-01 01:24:03 +0700

    unit test for table, date-time-pipe, error-handler, product-state

--- src/app/shared/services/error-handler/error-handler.spec.ts
+++ src/app/shared/services/error-handler/error-handler.spec.ts
@@ -1,16 +1,77 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ErrorHandler } from './error-handler';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('ErrorHandler', () => {
   let service: ErrorHandler;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ]
+    });
     service = TestBed.inject(ErrorHandler);
+    spyOn(console, 'error');
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should navigate to /error/404 for 404 error (not email check)', () => {
+    const error = new HttpErrorResponse({
+      status: 404,
+      url: '/some-other-endpoint'
+    });
+    service.handleError(error);
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/error/404']);
+  });
+  it('should NOT navigate for 404 error with email check', () => {
+    const error = new HttpErrorResponse({
+      status: 404,
+      url: '/user?email=test@example.com'
+    });
+    service.handleError(error);
+    expect(routerSpy.navigate).not.toHaveBeenCalled();
+  });
+
+  it('should navigate to /error/500 for 500 error', () => {
+    const error = new HttpErrorResponse({
+      status: 500,
+      url: '/api/data'
+    });
+    service.handleError(error);
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/error/500']);
+  });
+
+  it('should navigate to /error/no-connection for status 0', () => {
+    spyOn((service as any), 'showNetworkError');
+
+    const error = new HttpErrorResponse({
+      status: 0,
+      url: '/api/data'
+    });
+    service.handleError(error);
+    expect((service as any).showNetworkError).toHaveBeenCalled();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/error/no-connection']);
+  });
+
+  it('should navigate to /error/500 for unknown status', () => {
+    const error = new HttpErrorResponse({
+      status: 403,
+      url: '/api/data'
+    });
+    service.handleError(error);
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/error/500']);
+  });
+
+  it('should throw "Method not implemented." error when showNetworkError is called', () => {
+    expect(() => service['showNetworkError']()).toThrowError('Method not implemented.');
+  });  
 });
--- src/app/shared/services/state/product-state.spec.ts
+++ src/app/shared/services/state/product-state.spec.ts
@@ -1,16 +1,76 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductState } from './product-state';
+import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
+import { ProductService } from '../product-service/product-service';
+import { Loading } from '../loading/loading';
+import { customMessageService } from '../message-service/message-service';
+import { signal } from '@angular/core';
+import { of, throwError } from 'rxjs';
+import { Order } from '../../models/order.model';
 
 describe('ProductState', () => {
   let service: ProductState;
+  let productServiceSpy: jasmine.SpyObj<ProductService>;
+  let loadingSpy: jasmine.SpyObj<Loading>;
+  let messageServiceSpy: jasmine.SpyObj<customMessageService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    productServiceSpy = jasmine.createSpyObj('ProductService', [ 'getOrder' ]);
+    loadingSpy = jasmine.createSpyObj('Loading', [ 'show', 'hide' ]);
+    messageServiceSpy = jasmine.createSpyObj('customMessageService', ['showError']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        MessageService,
+        { provide: ProductService, useValue: productServiceSpy },
+        { provide: Loading, useValue: loadingSpy },
+        { provide: customMessageService, useValue: messageServiceSpy }
+      ]
+    });
     service = TestBed.inject(ProductState);
+    service.reccentSales = signal([]);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should load orders and update reccentSales signal', () => {
+    const mockOrders = structuredClone([
+      {
+        cart: [
+          {
+            productId: '1',
+            productName: 'Pate',
+            unitPrice: 15,
+            quantity: 2,
+            imageUrl: ''
+          }
+        ]
+      }
+    ]) as Order[];    
+    
+    productServiceSpy.getOrder.and.returnValue(of(mockOrders));
+    service.loadOrder();
+    expect(loadingSpy.show).toHaveBeenCalled();
+    expect(productServiceSpy.getOrder).toHaveBeenCalled();
+  
+    expect(service.reccentSales()).toEqual([
+      mockOrders[0].cart[0],
+    ]);
+    expect(loadingSpy.hide).toHaveBeenCalled();
+  })
+
+  it('should show error when getOrder fails', () => {
+    productServiceSpy.getOrder.and.returnValue(throwError(() => new Error('Failed')));
+    service.loadOrder();
+  
+    expect(loadingSpy.show).toHaveBeenCalled();
+    expect(messageServiceSpy.showError).toHaveBeenCalledWith('Error', 'Unable to load cart data');
+    expect(loadingSpy.hide).toHaveBeenCalled();
+  });
+  
 });
